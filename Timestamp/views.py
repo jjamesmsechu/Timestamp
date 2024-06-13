935e4eca4f41de016b443d3d20152d0b7a01bc6d
jjamesmsechu <jjamesmsechu@gmail.com>
2024-06-13 17:09:30 +0300

    Adding styling to title just before leaving work

--- Timestamp/views.py
+++ Timestamp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request,'Timestamp/home.html')
+def Hours(request):
+    return render(request,'Timestamp/hours.html')
